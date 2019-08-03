a95c985db2fd02dcd710c3a91b67a9b472c9f04f
pandzic.ivan <pandzic.ivan@nsoft.com>
2019-08-03 22:28:22 +0200

    Add material ui and create Register component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import indigo from '@material-ui/core/colors/indigo';
+import deepOrange from '@material-ui/core/colors/deepOrange';
+import red from '@material-ui/core/colors/red';
+import Register from './pages/register';
 import logo from './logo.svg';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: indigo,
+    secondary: deepOrange,
+    error: red,
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <Register />
+      </div>
+    </ThemeProvider>
   );
 }
 
--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,7 +1,54 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import style from './register.module.scss';
 
 class Register extends React.Component {
-  constructor(props) {
-    super(props);
+  render() {
+    return (
+      <div className={style.register}>
+        <TextField
+          margin="dense"
+          label="translation.first_name"
+          variant="outlined"
+          fullWidth
+        />
+        <TextField
+          margin="dense"
+          label="translation.last_name"
+          variant="outlined"
+          fullWidth
+        />
+        <TextField
+          margin="dense"
+          label="translation.email"
+          variant="outlined"
+          fullWidth
+        />
+        <TextField
+          margin="dense"
+          label="translation.password"
+          type="password"
+          variant="outlined"
+          fullWidth
+        />
+        <TextField
+          margin="dense"
+          label="translation.repeat_password"
+          type="password"
+          variant="outlined"
+          fullWidth
+        />
+        <Button color="primary"
+          className={style.registerBtn}
+          variant="contained"
+          size="large"
+          fullWidth>
+          translation.register
+        </Button>
+      </div>
+    )
   }
 }
+
+export default Register;
