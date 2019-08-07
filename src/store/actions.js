64c1049721358980fb0a31c54c7173878577cdc4
pandzic.ivan <pandzic.ivan@nsoft.com>
2019-08-07 22:09:49 +0200

    Add redux to app

---
+++ src/store/actions.js
@@ -0,0 +1,7 @@
+export const GET_TRANSLATIONS = 'GET_TRANSLATIONS';
+
+export function getTranslations() {
+  return {
+    type: GET_TRANSLATIONS,
+  }
+}
---
+++ src/store/reducers.js
@@ -0,0 +1,25 @@
+import { GET_TRANSLATIONS } from './actions';
+
+const initialState = {
+  translations: {
+    register_first_name: 'First name',
+    register_last_name: 'Last name',
+    register_email: 'Email',
+    register_password: 'Password',
+    register_repeat_password: 'Repeat password',
+    register_register: 'Register',
+    form_field_required: 'Field is required', 
+    form_field_min_length: 'Minimal length is',
+    form_field_password_mismatch: 'Passwords do not match',
+    form_field_invalid_email: 'Invalid email',
+  }
+}
+
+export default function(state = initialState, action) {
+  switch (action) {
+    case GET_TRANSLATIONS:
+      return state;
+    default:
+      return state;
+  }
+}
