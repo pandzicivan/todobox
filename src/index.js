32e260281832245cb97a84fb5c8c29cdb0c233c5
ipandzic <ivan.pandzic1989@gmail.com>
2020-07-20 10:27:20 +0200

    Move app component to folder

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {Provider} from 'react-redux';
+import {store} from './store/store';
+import AppContainer from './containers/AppContainer';
 import * as serviceWorker from './serviceWorker';
 import './styles/global.scss';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <AppContainer />
+  </Provider>, document.getElementById('root')
+);
 
 serviceWorker.unregister();
