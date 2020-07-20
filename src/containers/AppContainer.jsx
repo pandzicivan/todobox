3904971fb7b8a2cf3c31a32c0b3b432e64d1aa59
ipandzic <ivan.pandzic1989@gmail.com>
2020-07-20 10:31:17 +0200

    Add App and App container under VSC

---
+++ src/containers/AppContainer.jsx
@@ -0,0 +1,19 @@
+import {connect} from 'react-redux';
+import App from '../components/App';
+import {checkAuth} from '../store/actions';
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    checkAuth() {
+      dispatch(checkAuth());
+    },
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
