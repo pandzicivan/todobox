b531d47db97080f60946f338309ecce167d9ba86
ipandzic <ivan.pandzic1989@gmail.com>
2020-07-21 12:44:59 +0200

    Add hooks for sidebar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
+import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+
 import style from './style.module.scss';
 import Logo from '../Logo';
 
 const Header = () => {
+  const [drawer, toggleDrawer] = useState(false);
+
   return(
     <div className={style.header}>
       <div className={style.logo}>
         <Logo />
       </div>
-      <div className={style.profile}>
-        <span className={`${style.icon} material-icons`}>
-          person
-        </span>
-      </div>
+      <React.Fragment>
+        <div className={style.profile}
+          onClick={() => toggleDrawer(!drawer)}>
+          <span className={`${style.icon} material-icons`}>
+            person
+          </span>
+        </div>
+        <SwipeableDrawer
+          anchor={"right"}
+          open={drawer}
+          onClose={() => toggleDrawer(false)}
+          onOpen={() => toggleDrawer(true)}
+        >
+          <h1>Hehe</h1>
+        </SwipeableDrawer>
+      </React.Fragment>
     </div>
   )
 }
